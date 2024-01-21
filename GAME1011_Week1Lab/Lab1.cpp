a8ab25816dff81b530705f80c00c82412d117478
Daniel Bassili <101467288@georgebrown.ca>
2024-01-21 12:49:26 -0500

    Lab 1 completely changed to be able to add platforms, games, and achievements

--- GAME1011_Week1Lab/Lab1.cpp
+++ GAME1011_Week1Lab/Lab1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 using namespace std;
 
 class Achievement
@@ -7,7 +9,7 @@ class Achievement
 public:
     Achievement(string title, string description, int value) : title(title), description(description), value(value)
     {
-    
+
     }
 
     void displayAchievement() const
@@ -24,134 +26,351 @@ private:
 class Game
 {
 public:
-    Game(string gameName, string gamePublisher, string gameDeveloper, Achievement* achievementsArray)
-        : gameName(gameName), gamePublisher(gamePublisher), gameDeveloper(gameDeveloper)
+    Game(string gameName, string gamePublisher, string gameDeveloper, const vector<Achievement>& achievements)
+        : gameName(gameName), gamePublisher(gamePublisher), gameDeveloper(gameDeveloper), achievements(achievements)
     {
-        this->achievementsArray = achievementsArray;
+
     }
 
-    ~Game()
+    string getGameName() const
     {
-        delete[] achievementsArray;
+        return gameName;
     }
 
     void displayGameDetails() const
     {
         cout << "\nGame: " << gameName << "\n  Publisher: " << gamePublisher << "\n  Developer: " << gameDeveloper << "\n";
         cout << "  Achievements:\n";
-        for (int i = 0; i < 5; i++)
+        for (const auto& achievement : achievements)
         {
-            achievementsArray[i].displayAchievement();
+            achievement.displayAchievement();
         }
+    }
+
+    void addAchievement(const Achievement& achievement)
+    {
+        achievements.push_back(achievement);
     }
 
 private:
     string gameName;
     string gamePublisher;
     string gameDeveloper;
-    Achievement* achievementsArray;
+    vector<Achievement> achievements;
 };
 
 class Platform
 {
 public:
-    Platform(string n, string m, string* gamesArray) : platformName(n), platformManufacturer(m)
+    Platform(string n, string m, const vector<string>& gamesVector) : platformName(n), platformManufacturer(m), gamesVector(gamesVector)
     {
-        this->gamesArray = new string[5];
-        for (int i = 0; i < 5; ++i)
-        {
-            this->gamesArray[i] = gamesArray[i];
-        }
-        cout << "Platform: " << platformName << "\nBy " << platformManufacturer << "\n";
+
     }
 
     ~Platform()
     {
-        delete[] gamesArray;
+
+    }
+
+    string getPlatformName() const
+    {
+        return platformName;
+    }
+
+    void displayPlatformDetails() const
+    {
+        cout << "\nPlatform Details:\n";
+        cout << "  Name: " << platformName << "\n  Manufacturer: " << platformManufacturer << "\n";
+        cout << "  Games Available:\n";
+        for (size_t i = 0; i < gamesVector.size(); i++)
+        {
+            cout << "    " << i + 1 << ". " << gamesVector[i] << "\n";
+        }
     }
 
 private:
     string platformName;
     string platformManufacturer;
-    string* gamesArray;
+    vector<string> gamesVector;
 };
+
+void createNewPlatform(vector<Platform>& platforms)
+{
+    string name, manufacturer;
+    cout << "Enter platform name: ";
+    cin.ignore();
+    getline(cin, name);
+    cout << "Enter platform manufacturer: ";
+    getline(cin, manufacturer);
+
+    vector<string> gamesVector(5);
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "Enter game " << i + 1 << " name for the platform: ";
+        getline(cin, gamesVector[i]);
+    }
+
+    Platform* newPlatform = new Platform(name, manufacturer, gamesVector);
+
+    platforms.push_back(*newPlatform);
+
+    cout << "New platform created successfully!\n";
+}
+
+void createNewGame(vector<Game>& gameList, vector<Achievement>& achievementsList, const vector<Platform>& platforms)
+{
+    string name, publisher, developer;
+    int platformIndex;
+
+    cout << "Enter game name: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    getline(cin, name);
+    cout << "Enter game publisher: ";
+    getline(cin, publisher);
+    cout << "Enter game developer: ";
+    getline(cin, developer);
+
+    cout << "Available platforms:\n";
+    for (size_t i = 0; i < platforms.size(); i++)
+    {
+        cout << i + 1 << ". " << platforms[i].getPlatformName() << "\n";
+    }
+
+    cout << "Select the platform for the game (enter the number): ";
+    if (!(cin >> platformIndex) || platformIndex < 1 || platformIndex > static_cast<int>(platforms.size()))
+    {
+        cout << "Invalid selection. Game creation canceled.\n";
+        return;
+    }
+
+    Game newGame(name, publisher, developer, {});
+
+    for (int i = 0; i < 5; ++i)
+    {
+        int achievementIndex;
+        cout << "Select achievement " << i + 1 << " for the game (enter the number): ";
+        if (!(cin >> achievementIndex) || achievementIndex < 1 || achievementIndex > static_cast<int>(achievementsList.size()))
+        {
+            cout << "Invalid selection. Game creation canceled.\n";
+            return;
+        }
+        newGame.addAchievement(achievementsList[achievementIndex - 1]);
+    }
+
+    gameList.push_back(newGame);
+    cout << "New game created successfully!\n";
+}
+
+void createNewAchievement(vector<Achievement>& achievementsList)
+{
+    string title;
+    string description;
+    int value;
+
+    cout << "Enter achievement title: ";
+    cin.ignore();
+    getline(cin, title);
+    cout << "Enter achievement description: ";
+    getline(cin, description);
+    cout << "Enter achievement value: ";
+    cin >> value;
+
+    Achievement newAchievement(title, description, value);
+    achievementsList.push_back(newAchievement);
+    cout << "New achievement created successfully!\n";
+}
+
+void viewPlatformDetails(const vector<Platform>& platforms)
+{
+    cout << "Available platforms:\n";
+    for (size_t i = 0; i < platforms.size(); i++)
+    {
+        cout << i + 1 << ". " << platforms[i].getPlatformName() << "\n";
+    }
+
+    int platformIndex;
+    cout << "Select the platform to view details (enter the number): ";
+    if (!(cin >> platformIndex) || platformIndex < 1 || platformIndex > static_cast<int>(platforms.size()))
+    {
+        cout << "Invalid selection. Returning to the main menu.\n";
+        return;
+    }
+
+    platforms[platformIndex - 1].displayPlatformDetails();
+}
+
+void viewGameDetails(const vector<Game>& gameList)
+{
+    cout << "Available games:\n";
+    for (size_t i = 0; i < gameList.size(); i++)
+    {
+        cout << i + 1 << ". " << gameList[i].getGameName() << "\n";
+    }
+
+    int gameIndex;
+    cout << "Select the game to view details (enter the number): ";
+    if (!(cin >> gameIndex) || gameIndex < 1 || gameIndex > static_cast<int>(gameList.size()))
+    {
+        cout << "Invalid selection. Returning to the main menu.\n";
+        return;
+    }
+
+    gameList[gameIndex - 1].displayGameDetails();
+}
+
+void viewAllAchievements(const vector<Achievement>& achievements)
+{
+    cout << "All Achievements:\n";
+    for (const auto& achievement : achievements)
+    {
+        achievement.displayAchievement();
+    }
+}
+
+int displayMainMenu()
+{
+    int choice;
+    cout << "\n--- Main Menu ---\n";
+    cout << "1. Create New Platform\n";
+    cout << "2. Create New Game\n";
+    cout << "3. Create New Achievement\n";
+    cout << "4. View Platform Details\n";
+    cout << "5. View Game Details and Achievements\n";
+    cout << "6. View All Achievements\n";
+    cout << "0. Exit\n";
+    cout << "Enter your choice: ";
+
+    if (!(cin >> choice) || choice < 0 || choice > 6)
+    {
+        cout << "Invalid choice. Exiting.\n";
+        return -1;
+    }
+
+    return choice;
+}
 
 int main()
 {
-    const int NUM_GAMES = 5;
-    string ps5Games[NUM_GAMES] = { "Call of Duty", "Horizon Forbidden West", "God of War 2", "Spiderman 2", "Rocket League" };
+    string ps5Games[5] = { "Call of Duty", "Horizon Forbidden West", "God of War 2", "Spiderman 2", "Rocket League" };
 
-    Platform ps5("Playstation 5", "Sony", ps5Games);
-
-    Achievement codAchievements[] = {
+    Platform ps5("Playstation 5", "Sony", vector<string>(ps5Games, ps5Games + sizeof(ps5Games) / sizeof(ps5Games[0])));
+    Achievement codAchievements[] =
+    {
         {"First Blood", "Achieve your first kill", 10},
         {"Veteran", "Reach level 50", 50},
         {"Master of War", "Complete all missions", 100},
         {"Sharpshooter", "Score 100 headshots", 30},
-        {"Collector", "Unlock all weapons", 75} };
+        {"Collector", "Unlock all weapons", 75}
+    };
 
-    Achievement hzdAchievements[] = {
+    Achievement hzdAchievements[] = 
+    {
         {"Explorer", "Visit all regions", 20},
         {"Machine Hunter", "Defeat 50 machines", 40},
         {"Lore Master", "Discover all datapoints", 60},
         {"Master Hunter", "Defeat all legendary machines", 80},
-        {"Platinum Trophy", "Unlock all other trophies", 100} };
+        {"Platinum Trophy", "Unlock all other trophies", 100}
+    };
 
-    Achievement dmcAchievements[] = {
+    Achievement dmcAchievements[] = 
+    {
         {"Stylish!", "Achieve SSS rank", 20},
         {"Combo Master", "Perform a 100-hit combo", 30},
         {"Demon Slayer", "Defeat 1000 demons", 50},
         {"Dante's Legacy", "Complete the game with Dante", 75},
-        {"Legendary Devil Hunter", "Complete the game on Dante Must Die difficulty", 100} };
+        {"Legendary Devil Hunter", "Complete the game on Dante Must Die difficulty", 100}
+    };
 
-    Achievement spdAchievements[] = {
+    Achievement spdAchievements[] = 
+    {
         {"Web Slinger", "Travel 100 miles using webs", 20},
         {"Arachnid Avenger", "Defeat 500 enemies", 40},
         {"City Protector", "Complete all crimes in the city", 60},
         {"Wall Crawler", "Climb 100 buildings", 30},
-        {"Spectacular Spider-Man", "Complete all challenges", 100} };
+        {"Spectacular Spider-Man", "Complete all challenges", 100}
+    };
 
-    Achievement rlAchievements[] = {
+    Achievement rlAchievements[] = 
+    {
         {"First Goal", "Score your first goal", 10},
         {"Hat Trick", "Score three goals in a match", 30},
         {"Rocketeer", "Unlock all Rocket Trails", 50},
         {"Grand Champion", "Reach the Grand Champion rank", 80},
-        {"Supersonic Legend", "Reach the Supersonic Legend rank", 100} };
+        {"Supersonic Legend", "Reach the Supersonic Legend rank", 100}
+    };
 
-    Game games[] = {
-        Game("Call of Duty", "Activision", "Sledgehammer Games", codAchievements),
-        Game("Horizon Forbidden West", "Sony", "Guerrilla Games", hzdAchievements),
-        Game("Devil May Cry", "Capcom", "Capcom", dmcAchievements),
-        Game("Spiderman 2", "Sony", "Insomniac Games", spdAchievements),
-        Game("Rocket League", "Psyonix", "Psyonix", rlAchievements) };
 
-    while (true) {
-        // Display the list of games
-        cout << "\n\nGames available:\n\n";
-        for (int i = 0; i < NUM_GAMES; i++) {
-            cout << i + 1 << ". " << ps5Games[i] << "\n";
-        }
+    vector<Platform> platforms;
+    vector<Game> gameList;
+    vector<Achievement> achievementsList;
+    vector<Achievement> achievements;
 
-        // Player picks a game
-        int selectedGame;
-        cout << "\nEnter the number of the game you want to view (or enter 0 to exit): ";
-        if (!(cin >> selectedGame) || cin.fail()) {
-            cout << "Invalid input. Exiting.\n";
-            return 1; // Exit with an error code
-        }
+    vector<Achievement> codAchievementsVec(codAchievements, codAchievements + sizeof(codAchievements) / sizeof(Achievement));
+    vector<Achievement> hzdAchievementsVec(hzdAchievements, hzdAchievements + sizeof(hzdAchievements) / sizeof(Achievement));
+    vector<Achievement> dmcAchievementsVec(dmcAchievements, dmcAchievements + sizeof(dmcAchievements) / sizeof(Achievement));
+    vector<Achievement> spdAchievementsVec(spdAchievements, spdAchievements + sizeof(spdAchievements) / sizeof(Achievement));
+    vector<Achievement> rlAchievementsVec(rlAchievements, rlAchievements + sizeof(rlAchievements) / sizeof(Achievement));
 
-        if (selectedGame == 0) {
+
+    Game codGame("Call of Duty", "Activision", "Infinity Ward", codAchievementsVec);
+    Game hzdGame("Horizon Forbidden West", "Sony", "Guerrilla Games", hzdAchievementsVec);
+    Game dmcGame("Devil May Cry 5", "Capcom", "Capcom", dmcAchievementsVec);
+    Game spdGame("Spiderman 2", "Sony", "Insomniac Games", spdAchievementsVec);
+    Game rlGame("Rocket League", "Psyonix", "Psyonix", rlAchievementsVec);
+
+
+    // Adding Games to the gameList vector
+    gameList.push_back(codGame);
+    gameList.push_back(hzdGame);
+    gameList.push_back(dmcGame);
+    gameList.push_back(spdGame);
+    gameList.push_back(rlGame);
+
+    // Adding Achievements to the achievements vector
+    achievements.insert(achievements.end(), codAchievementsVec.begin(), codAchievementsVec.end());
+    achievements.insert(achievements.end(), hzdAchievementsVec.begin(), hzdAchievementsVec.end());
+    achievements.insert(achievements.end(), dmcAchievementsVec.begin(), dmcAchievementsVec.end());
+    achievements.insert(achievements.end(), spdAchievementsVec.begin(), spdAchievementsVec.end());
+    achievements.insert(achievements.end(), rlAchievementsVec.begin(), rlAchievementsVec.end());
+
+    while (true)
+    {
+        int choice = displayMainMenu();
+
+        switch (choice)
+        {
+        case 0:
             cout << "Exiting program. Goodbye!\n";
-            break; // Exit the loop
-        }
+            return 0;
 
-        if (selectedGame < 1 || selectedGame > NUM_GAMES) {
-            cout << "Invalid selection. Please try again.\n";
-            continue; // Go to the next iteration of the loop
-        }
+        case 1:
+            createNewPlatform(platforms);
+            break;
 
-        // Display the details of the selected game
-        games[selectedGame - 1].displayGameDetails();
+        case 2:
+            createNewGame(gameList, achievementsList, platforms);
+            break;
+
+        case 3:
+            createNewAchievement(achievementsList);
+            break;
+
+        case 4:
+            viewPlatformDetails(platforms);
+            break;
+
+        case 5:
+            viewGameDetails(gameList);
+            break;
+
+        case 6:
+            viewAllAchievements(achievements);
+            break;
+
+        default:
+            cout << "Invalid choice. Please try again.\n";
+            break;
+        }
     }
 
     return 0;
