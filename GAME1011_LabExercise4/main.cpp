f636a1e0c55ba99651a56cd8e2487fe1618a8277
Daniel Bassili <thedagger994@gmail.com>
2024-04-03 12:24:35 -0400

    Lab Exercise 4 complete

--- GAME1011_LabExercise4/main.cpp
+++ GAME1011_LabExercise4/main.cpp
@@ -19,3 +19,22 @@ string ReverseString (string str, int start, int end)
     swap (str[start], str[end]);
     return ReverseString (str, start + 1, end - 1);
 }
+int main ()
+{
+    string input;
+
+    //Check for a Palindrome
+    cout << "Enter a string to check if it's a palindrome:" << endl;
+    getline (cin, input);
+    if (IsPalindrome (input, 0, input.length () - 1))
+        cout << "\"" << input << "\" is a palindrome." << endl;
+    else
+        cout << "\"" << input << "\" is not a palindrome." << endl;
+
+    //Reverse a given string
+    cout << "Enter a string to reverse: ";
+    getline (cin, input);
+    cout << "Reversed string: " << ReverseString (input, 0, input.length () - 1) << endl;
+
+    return 0;
+}
